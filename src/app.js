f44bb5fba5bc013fb5fbc2c3afa61a8c5d402728
carlosvldz <me@carlosvldz.com>
2020-12-14 10:55:06 -0600

    add error handling and pagination for get all route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 import TasksRoutes from './routes/tasks.routes'
 
 const app = express()
@@ -6,13 +8,18 @@ const app = express()
 // settings
 app.set('port', process.env.PORT || 3000);
 
+// middlewares
+const corsOptions = {};
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // routes
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to my Notes App'})
+    res.json({ message: 'Welcome to my Notes App' })
 })
 
-app.use('/api/tasks',TasksRoutes)
+app.use('/api/tasks', TasksRoutes)
 
 export default app;
--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,34 +1,82 @@
 import Task from '../models/Task'
+import { getPagination } from '../libs/getPagination'
 
 export const findAllTasks = async (req, res) => {
-    const tasks = await Task.find()
-    res.json(tasks)
+    try {
+        const { size, page, title } = req.query
+
+        const condition = title
+            ? {
+                title: { $regex: new RegExp(title), $options: "i" }
+            }
+            : {};
+
+        const { limit, offset } = getPagination(page, size)
+
+        const data = await Task.paginate(condition, { offset, limit })
+        res.json({
+            totalItems: data.totalDocs,
+            tasks: data.docs,
+            totalPages: data.totalPages,
+            currentPage: data.page - 1
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || "Something goes wrong retrieving the tasks"
+        })
+    }
 }
 
 export const createTask = async (req, res) => {
-    const newTask = new Task({
-        title: req.body.title,
-        description: req.body.description,
-        done: req.body.done ? req.body.done : false
-    });
-    const taskSaved = await newTask.save();
-    res.json(taskSaved)
-} 
+
+    if (!req.body.title) {
+        return res.status(400).send({ message: 'Content cannot be empty' })
+    }
+
+    try {
+        const newTask = new Task({
+            title: req.body.title,
+            description: req.body.description,
+            done: req.body.done ? req.body.done : false
+        });
+        const taskSaved = await newTask.save();
+        res.json(taskSaved)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || "Something goes wrong creating a task"
+        })
+    }
+}
 
 export const findOneTask = async (req, res) => {
-    const task = await Task.findById(req.params.id)
-    res.json(task)
+    const { id } = req.params;
+    try {
+        const task = await Task.findById(id)
+        if (!task) return res.status(404).json({ message: `Task with id ${id} does not exists!` })
+        res.json(task)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || `Error retrieving task with id: ${id}`
+        })
+    }
 }
 
 export const deleteTask = async (req, res) => {
-    const data = await Task.findByIdAndDelete(req.params.id)
-    res.json({
-        message: `${data.title} - Task were deleted successfully!`
-    })
+    const { id } = req.params;
+    try {
+        const data = await Task.findByIdAndDelete(id)
+        res.json({
+            message: `${data.title} - Task were deleted successfully!`
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: `Cannot delete task with id: ${id}`
+        })
+    }
 }
 
 export const findAllDoneTasks = async (req, res) => {
-    const tasks = await Task.find({done: true});
+    const tasks = await Task.find({ done: true });
     res.json(tasks);
 }
 
